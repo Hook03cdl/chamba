a8312f953f19e56e8964c954fc71d5c0904ebbd7
Hook03cdl <carlosduverney@gmail.com>
2024-08-26 20:02:59 -0700

    mejoras de componentes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,35 +1,15 @@
-'use client';
-
 import Logo from '@/components/ui/Logo';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/Inputs';
 import Link from 'next/link';
-import { useState } from 'react';
-import { Eye, EyeOff } from 'lucide-react';
 
 export default function Login() {
-	const [visible, setVisible] = useState(false);
-	const [eyeVisible, setEyeVisible] = useState('hidden');
-	const [eyeOffVisible, setEyeOffVisible] = useState('');
-
-	function changePassword() {
-		setVisible(!visible);
-
-		if (visible) {
-			setEyeVisible('hidden');
-			setEyeOffVisible('');
-		} else {
-			setEyeVisible('');
-			setEyeOffVisible('hidden');
-		}
-	}
-
 	return (
 		<section className="flex *:flex-1 *:h-svh">
-			<section className="bg-humo flex justify-center items-center">
+			<article className="bg-humo flex justify-center items-center">
 				<Logo variant="dark" />
-			</section>
-			<section className="bg-shark flex justify-center items-center flex-col gap-5">
+			</article>
+			<article className="bg-shark flex justify-center items-center flex-col gap-5">
 				<div className="space-y-10">
 					<Input
 						label={'Correo electrónico'}
@@ -49,7 +29,7 @@ export default function Login() {
 				<Link href={'/pruebas'} className=" text-humo hover:underline">
 					¿No tienes una cuenta?
 				</Link>
-			</section>
+			</article>
 		</section>
 	);
 }
