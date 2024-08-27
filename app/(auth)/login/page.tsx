36de7df0b41aefd03f66a189a9e2193a5e30b126
jonathanj20 <jonathanpossada@gmail.com>
2024-08-26 19:02:41 -0700

    Se puede mostrar y ocultar la contraseña en el login

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,24 +1,52 @@
-import Logo from '@/components/ui/Logo';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/Inputs';
-import Link from 'next/link';
+'use client';
 
-export default function Login() {
-	return (
-		<section className="flex h-svh *:h-full">
-			<article className="bg-humo flex-1 flex justify-center items-center">
-				<Logo variant="dark" />
-			</article>
-			<article className="bg-shark flex-1 flex justify-center items-center flex-col gap-10">
-				<div className="space-y-10">
-					<Input label={'Correo electrónico'} id="prueba2" required />
-					<Input label={'Contraseña'} id="prueba" required />
-					<Button className="w-full">Iniciar sesión</Button>
-				</div>
-				<Link href={'/signup'} className="text-humo hover:underline">
-					¿No tienes una cuenta?
-				</Link>
-			</article>
-		</section>
-	);
+import Logo from "@/components/ui/Logo";
+import { Button } from "@/components/ui/button";
+import { Input } from '@/components/ui/Inputs';
+import Link from "next/link";
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+
+export default function Login(){
+    const [visible, setVisible] = useState(false);
+    const [eyeVisible, setEyeVisible] = useState('hidden');
+    const [eyeOffVisible, setEyeOffVisible] = useState('');
+
+    function changePassword(){
+        setVisible(!visible);
+        
+        if(visible){
+            setEyeVisible('hidden');
+            setEyeOffVisible('');
+        } else {
+            setEyeVisible('')
+            setEyeOffVisible('hidden');
+        }
+    }
+
+    return(
+        <section className="flex">
+            <section className="bg-[#D9D9D9] w-3/6 h-[100vh] flex justify-center items-center">
+                <Logo></Logo>
+            </section>
+            <section className="bg-[#212322] w-3/6 flex justify-center items-center flex-col">
+                <div className="space-y-10">
+                    <Input label={'Correo electrónico'} id="prueba2" required/>
+                    <div className="relative">
+                        <Input label={'Contraseña'} id="prueba" type={visible ? 'text' : 'password'} required/>
+                        
+                        <button onClick={changePassword} className={`${eyeVisible} absolute right-1 bottom-1`}>
+                            <Eye color="white" />
+                        </button>
+
+                        <button onClick={changePassword} className={`${eyeOffVisible} absolute right-1 bottom-1`}>
+                            <EyeOff color="white" />
+                        </button>
+                    </div>
+                    <Button className="w-full">Iniciar sesión</Button>
+                </div>
+                <Link href={'/pruebas'} className="mt-14 text-white hover:underline">¿No tienes una cuenta?</Link>
+            </section>
+        </section>
+    );
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Logo from '@/components/ui/Logo';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/Inputs';
+import Link from 'next/link';
 
 export default function page() {
-	return <div></div>;
+	return(
+		<section>
+			
+		</section>
+	);
 }
