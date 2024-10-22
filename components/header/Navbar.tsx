b5e86a397b1075addfebeb9730cbf52e6679ebbc
adalbertog20 <adalbertog_20@alu.uabcs.mx>
2024-10-21 20:17:33 -0700

    banner for sign up

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -5,27 +5,36 @@ import Notifications from './Notifications';
 import Link from 'next/link';
 import { getToken } from '@/lib/utils/tokens';
 import { Gem } from 'lucide-react';
+import { fetchDataUser } from '@/lib/data/user';
 
 export default async function Navbar() {
 	const session = await getToken('session');
+	const user = await fetchDataUser();
 	return (
-		<header className="sticky z-40 top-0 bg-humo">
-			<div className="flex justify-between items-center p-3 px-10 shadow-md">
-				<Logo size="small" variant="dark" />
-				<InputSearch />
-				{/* <NavChambas /> */}
-				<div className="flex gap-3 items-center">
-					{session && (
-						<>
-							<Link href={'/suscribirse'} className="text-gray-600">
-								<Gem size={28} />
-							</Link>
-							<Notifications />
-						</>
-					)}
-					<Avatar />
-				</div>
-			</div>
-		</header>
-	);
+    <header className="sticky z-40 top-0 bg-humo">
+      <div className="flex justify-between items-center p-3 px-10 shadow-md">
+        <Logo size="small" variant="dark" />
+        <InputSearch />
+        {/* <NavChambas /> */}
+        <div className="flex gap-3 items-center">
+          {session && (
+            <>
+              <Link href={"/suscribirse"} className="text-gray-600">
+                <Gem size={28} />
+              </Link>
+              <Notifications />
+            </>
+          )}
+          <Avatar />
+        </div>
+      </div>
+	  {!user &&
+		<div className="bg-niagara-700 p-1">
+			<h1 className="text-center text-white uppercase leading-normal">
+			<strong><a className='hover:underline' href='/signup'>Crea una cuenta</a></strong> para poder utilizar la aplicacion.
+			</h1>
+		</div>
+	  }
+    </header>
+  );
 }
