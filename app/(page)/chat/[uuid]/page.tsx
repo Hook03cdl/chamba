c5748ca5fe5fa6de59452dfeba60d1bfcd41fe2f
adalbertog20 <adalbertog_20@alu.uabcs.mx>
2024-11-10 00:34:13 -0700

    Implement notifications when sending messages

--- app/(page)/chat/[uuid]/page.tsx
+++ app/(page)/chat/[uuid]/page.tsx
@@ -5,6 +5,7 @@ import {useEffect, useState} from "react";
 import {MessageProps} from "@/lib/interfaces/interface";
 import {getChatMessages} from "@/lib/data/chats";
 import Each from "@/components/Each";
+import {storeMessage} from "@/lib/actions/chats";
 
 {/*
     TODO Implementar el chat que fetchee los mensajes cada 5 segundos
@@ -13,6 +14,7 @@ import Each from "@/components/Each";
 
 export default function Page() {
     const [messages, setMessages] = useState<MessageProps[]>([]);
+    const [newMessage, setNewMessage] = useState<string>("");
     const params = useParams();
 
     useEffect(() => {
@@ -27,6 +29,15 @@ export default function Page() {
         }, 2000 * 5);
         return () => clearInterval(intervalId);
     }, [params.uuid]);
+
+    const handleMessage = async () => {
+        if (newMessage.trim() !== "") {
+            await storeMessage(String(params.uuid), newMessage);
+            setNewMessage("");
+            const updatedMessages = await getChatMessages(String(params.uuid));
+            setMessages(updatedMessages);
+        }
+    }
 
     return (
         <div>
@@ -46,8 +57,9 @@ export default function Page() {
                 )}
             </div>
             <div>
-                <input type="text"/>
-                <button>Send</button>
+                <input type="text" value={newMessage} onChange={(e) => setNewMessage(e.target.value)}/>
+                <p>{newMessage}</p>
+                <button onClick={handleMessage}>Send</button>
             </div>
         </div>
     );
--- lib/actions/chats.ts
+++ lib/actions/chats.ts
@@ -2,8 +2,9 @@
 
 import {getToken} from "@/lib/utils/tokens";
 
-export async function storeMessage(chatUuid: String) {
+export async function storeMessage(chatUuid: String, body: String) {
     const session = await getToken("session");
+    console.log(JSON.stringify({body: body}));
 
     try {
         const response = await fetch(`http://localhost:8000/api/chat/${chatUuid}/messages`, {
@@ -11,9 +12,12 @@ export async function storeMessage(chatUuid: String) {
             headers: {
                 Authorization: `Bearer ${session}`,
                 Accept: "application/json",
-            }
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({body: body})
         })
         const data = await response.json();
+        console.log(data);
         return data;
     } catch (error) {
         console.error(error);
